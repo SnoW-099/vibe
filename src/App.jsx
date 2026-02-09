7295f63c6a7f315b43a3606f06cf4101a11792a7
Ryze099 <ryze0910@gmail.com>
2026-02-09 18:47:41 +0100

    style: add right-side orb for better visual balance

--- src/App.jsx
+++ src/App.jsx
@@ -200,6 +200,7 @@ function App() {
           <div className="orb orb-1"></div>
           <div className="orb orb-2"></div>
           <div className="orb orb-3"></div>
+          <div className="orb orb-4"></div>
         </div>
       )}
 
